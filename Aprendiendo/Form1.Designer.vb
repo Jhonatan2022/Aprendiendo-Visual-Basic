b9ac6f0737e012dd82c6bc58202fb86e5e71fe3a
Jhonattan Florez <Jdflorez038@misena.edu.co>
2023-03-12 11:47:51 +0100

    Aritmetica y comienzo en cambios de formulario

--- Aprendiendo/Form1.Designer.vb
+++ Aprendiendo/Form1.Designer.vb
@@ -22,17 +22,43 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        texto1 = New Label()
+        nombre = New TextBox()
         SuspendLayout()
         ' 
+        ' texto1
+        ' 
+        texto1.AutoSize = True
+        texto1.Location = New Point(16, 12)
+        texto1.Margin = New Padding(4, 0, 4, 0)
+        texto1.Name = "texto1"
+        texto1.Size = New Size(139, 20)
+        texto1.TabIndex = 0
+        texto1.Text = "Digite su nombre: "' 
+        ' nombre
+        ' 
+        nombre.Location = New Point(157, 8)
+        nombre.Margin = New Padding(4)
+        nombre.Name = "nombre"
+        nombre.Size = New Size(328, 32)
+        nombre.TabIndex = 1
+        nombre.Text = "Nombre Completo"' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(9F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.AppWorkspace
-        ClientSize = New Size(575, 352)
+        ClientSize = New Size(563, 522)
+        Controls.Add(nombre)
+        Controls.Add(texto1)
+        Font = New Font("MV Boli", 11.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Margin = New Padding(4)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents texto1 As Label
+    Friend WithEvents nombre As TextBox
 End Class
--- Aprendiendo/Form1.vb
+++ Aprendiendo/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    'Para Comentar Muchas lineas de código Ctrl + K +Ctrl + C
+    'Para Desomentar Muchas lineas de código Ctrl + K +Ctrl + U
+    Private Sub Form1_Load(sender As Object, e As EventArgs)
         'Esto genera un cuadro de error
         'Msgbox("Hola esto es un error", 'MsgboxStyle.Critical, "ERROR")
 
@@ -31,47 +33,70 @@
         'Msgbox("Ho", 'MsgboxStyle.'MsgboxRight, "")
 
 
-        'Declarando primeras variables en VB
-        Dim variable As Integer = 3 + 12
-        MsgBox(variable)
-        'Asignando nuevo valor a la variable
-        variable = 11
-        MsgBox(variable)
+        'declarando primeras variables en vb
+        'Dim variable As Integer = 3 + 12
+        'MsgBox(variable)
+        ''asignando nuevo valor a la variable
+        'variable = 11
+        'MsgBox(variable)
 
-        'Variables con decimales
-        Dim [decimal] As Double = 1.5
-        MsgBox([decimal])
-        [decimal] = 2.5
-        MsgBox([decimal])
+        ''Variables con decimales
+        'Dim [decimal] As Double = 1.5
+        'MsgBox([decimal])
+        '[decimal] = 2.5
+        'MsgBox([decimal])
 
-        'Variables con texto "String"
-        Dim texto As String = "Hola esto es texto"
-        MsgBox(texto)
-        Dim texto2 As String = "..."
-        MsgBox(texto + texto2)
+        ''Variables con texto "String"
+        'Dim texto As String = "Hola esto es texto"
+        'MsgBox(texto)
+        'Dim texto2 As String = "..."
+        'MsgBox(texto + texto2)
 
-        'Variables con fecha
-        Dim fecha As Date = "13-02-2023"
-        MsgBox(fecha)
-        'Otra forma de hacerlo
-        Dim fecha2 As Date = "02-JAN-2023"
-        MsgBox(fecha2)
+        ''Variables con fecha
+        'Dim fecha As Date = "13-02-2023"
+        'MsgBox(fecha)
+        ''Otra forma de hacerlo
+        'Dim fecha2 As Date = "02-JAN-2023"
+        'MsgBox(fecha2)
 
-        'Arrays en VB primero determinamos las cantidades
-        Dim arreglo(3) As String
-        arreglo(0) = "h"
-        arreglo(1) = "o"
-        arreglo(2) = "la"
-        'Imprimimos el resultado final
-        MsgBox(arreglo(0) + " " + arreglo(1) + "  " + arreglo(2))
+        ''Arrays en VB primero determinamos las cantidades
+        'Dim arreglo(3) As String
+        'arreglo(0) = "h"
+        'arreglo(1) = "o"
+        'arreglo(2) = "la"
+        ''Imprimimos el resultado final
+        'MsgBox(arreglo(0) + " " + arreglo(1) + "  " + arreglo(2))
 
-        'Arrays con posiciones
-        Dim posicion() As Integer
-        posicion = New Integer() {1, 2, 3}
-        MsgBox(posicion(0).ToString + "--" + posicion(1).ToString + "--" + posicion(2).ToString + "--")
+        ''Arrays con posiciones ToString convierte una variable en un objeto del tipo String
+        'Dim posicion() As Integer 'Integer es para enteros
+        'posicion = New Integer() {1, 2, 3}
+        'MsgBox(posicion(0).ToString + "--" + posicion(1).ToString + "--" + posicion(2).ToString + "--")
 
-        'Conservar los datos de las posiciones indcadas
-        ReDim Preserve posicion(2)
-        MsgBox(posicion(0).ToString + "--" + posicion(1).ToString + "--" + posicion(2).ToString)
+        ''Conservar los datos de las posiciones indcadas utilizar preserve
+        'ReDim Preserve posicion(2)
+        'MsgBox(posicion(0).ToString + "--" + posicion(1).ToString + "--" + posicion(2).ToString)
+
+        ''Creando matriz, se pueden usar maximo 32 dimenciones
+        'Dim matriz(,) As Integer = {{1, 2, 3}, {4, 5, 6}}
+        'MsgBox(matriz(0, 1).ToString)
+
+        ''OPPERADORES ARITMETICOS
+        'Dim numero, numero2, operacion As Integer
+        'numero = 2
+        'numero2 = 5
+        'operacion = numero + numero2
+        ''Tambien puede -, Mod (Modulo), /, *, += Para incrementar, -= Para decrementar
+        'MsgBox(operacion.ToString)
+
+        'Cambiando valores de formulario por código
+    End Sub
+
+    Private Sub nombre_TextChanged(sender As Object, e As EventArgs) Handles nombre.TextChanged
+
+        'Modificando formulario por medio de código
+        nombre.Text = "Nuevo texto"
+        nombre.TextAlign = HorizontalAlignment.Right
+        'Haciendo que el cuadro no sea modificable
+        nombre.ReadOnly = True
     End Sub
 End Class
