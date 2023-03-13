0036a1a9303a34887dcd597bfc92e069b4d8cc5d
Jhonattan Florez <Jdflorez038@misena.edu.co>
2023-03-14 00:02:49 +0100

    Programación de enventos en botones

--- Aprendiendo/Form1.Designer.vb
+++ Aprendiendo/Form1.Designer.vb
@@ -24,31 +24,158 @@ Partial Class Form1
     Private Sub InitializeComponent()
         texto1 = New Label()
         nombre = New TextBox()
+        boton = New Button()
+        numero1 = New TextBox()
+        numero2 = New TextBox()
+        sumar = New Button()
+        Label1 = New Label()
+        cierro = New Button()
+        igual = New Button()
+        diferente = New Button()
+        mayorque = New Button()
+        menorque = New Button()
+        mayorigual = New Button()
+        menorigual = New Button()
         SuspendLayout()
         ' 
         ' texto1
         ' 
         texto1.AutoSize = True
-        texto1.Location = New Point(16, 12)
+        texto1.Location = New Point(3, 16)
         texto1.Margin = New Padding(4, 0, 4, 0)
         texto1.Name = "texto1"
-        texto1.Size = New Size(139, 20)
+        texto1.Size = New Size(147, 20)
         texto1.TabIndex = 0
-        texto1.Text = "Digite su nombre: "' 
+        texto1.Text = "Nombre Completo: "' 
         ' nombre
         ' 
-        nombre.Location = New Point(157, 8)
+        nombre.Location = New Point(147, 13)
         nombre.Margin = New Padding(4)
         nombre.Name = "nombre"
         nombre.Size = New Size(328, 32)
         nombre.TabIndex = 1
         nombre.Text = "Nombre Completo"' 
+        ' boton
+        ' 
+        boton.Location = New Point(476, 12)
+        boton.Name = "boton"
+        boton.Size = New Size(75, 33)
+        boton.TabIndex = 2
+        boton.Text = "Guardar"
+        boton.UseVisualStyleBackColor = True
+        ' 
+        ' numero1
+        ' 
+        numero1.Location = New Point(3, 89)
+        numero1.Name = "numero1"
+        numero1.Size = New Size(100, 32)
+        numero1.TabIndex = 3
+        ' 
+        ' numero2
+        ' 
+        numero2.Location = New Point(109, 89)
+        numero2.Name = "numero2"
+        numero2.Size = New Size(100, 32)
+        numero2.TabIndex = 4
+        ' 
+        ' sumar
+        ' 
+        sumar.Location = New Point(215, 89)
+        sumar.Name = "sumar"
+        sumar.Size = New Size(75, 32)
+        sumar.TabIndex = 5
+        sumar.Text = "Sumar"
+        sumar.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(3, 66)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(231, 20)
+        Label1.TabIndex = 6
+        Label1.Text = "Digíte un número en cada casilla"' 
+        ' cierro
+        ' 
+        cierro.Location = New Point(296, 89)
+        cierro.Name = "cierro"
+        cierro.Size = New Size(75, 32)
+        cierro.TabIndex = 7
+        cierro.Text = "Cerrar"
+        cierro.UseVisualStyleBackColor = True
+        ' 
+        ' igual
+        ' 
+        igual.Location = New Point(3, 127)
+        igual.Name = "igual"
+        igual.Size = New Size(75, 28)
+        igual.TabIndex = 8
+        igual.Text = "Igual"
+        igual.UseVisualStyleBackColor = True
+        ' 
+        ' diferente
+        ' 
+        diferente.Location = New Point(109, 127)
+        diferente.Name = "diferente"
+        diferente.Size = New Size(81, 28)
+        diferente.TabIndex = 9
+        diferente.Text = "Diferente"
+        diferente.UseVisualStyleBackColor = True
+        ' 
+        ' mayorque
+        ' 
+        mayorque.Location = New Point(3, 161)
+        mayorque.Name = "mayorque"
+        mayorque.Size = New Size(93, 29)
+        mayorque.TabIndex = 10
+        mayorque.Text = "Mayor que"
+        mayorque.UseVisualStyleBackColor = True
+        ' 
+        ' menorque
+        ' 
+        menorque.Location = New Point(109, 161)
+        menorque.Name = "menorque"
+        menorque.Size = New Size(90, 29)
+        menorque.TabIndex = 11
+        menorque.Text = "Menor que"
+        menorque.UseVisualStyleBackColor = True
+        ' 
+        ' mayorigual
+        ' 
+        mayorigual.Location = New Point(3, 196)
+        mayorigual.Name = "mayorigual"
+        mayorigual.Size = New Size(100, 29)
+        mayorigual.TabIndex = 12
+        mayorigual.Text = "Mayor igual"
+        mayorigual.UseVisualStyleBackColor = True
+        ' 
+        ' menorigual
+        ' 
+        menorigual.Location = New Point(109, 196)
+        menorigual.Name = "menorigual"
+        menorigual.Size = New Size(100, 29)
+        menorigual.TabIndex = 13
+        menorigual.Text = "Menor igual"
+        menorigual.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(9F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.AppWorkspace
         ClientSize = New Size(563, 522)
+        Controls.Add(menorigual)
+        Controls.Add(mayorigual)
+        Controls.Add(menorque)
+        Controls.Add(mayorque)
+        Controls.Add(diferente)
+        Controls.Add(igual)
+        Controls.Add(cierro)
+        Controls.Add(Label1)
+        Controls.Add(sumar)
+        Controls.Add(numero2)
+        Controls.Add(numero1)
+        Controls.Add(boton)
         Controls.Add(nombre)
         Controls.Add(texto1)
         Font = New Font("MV Boli", 11.25F, FontStyle.Regular, GraphicsUnit.Point)
@@ -61,4 +188,16 @@ Partial Class Form1
 
     Friend WithEvents texto1 As Label
     Friend WithEvents nombre As TextBox
+    Friend WithEvents boton As Button
+    Friend WithEvents numero1 As TextBox
+    Friend WithEvents numero2 As TextBox
+    Friend WithEvents sumar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cierro As Button
+    Friend WithEvents igual As Button
+    Friend WithEvents diferente As Button
+    Friend WithEvents mayorque As Button
+    Friend WithEvents menorque As Button
+    Friend WithEvents mayorigual As Button
+    Friend WithEvents menorigual As Button
 End Class
--- Aprendiendo/Form1.vb
+++ Aprendiendo/Form1.vb
@@ -3,25 +3,25 @@
     'Para Desomentar Muchas lineas de código Ctrl + K +Ctrl + U
     Private Sub Form1_Load(sender As Object, e As EventArgs)
         'Esto genera un cuadro de error
-        'Msgbox("Hola esto es un error", 'MsgboxStyle.Critical, "ERROR")
+        'Msgbox("Hola esto es un error", MsgboxStyle.Critical, "ERROR")
 
         'Esto genera un cuadro de alerta
-        'Msgbox("Hola esto es una alerta", 'MsgboxStyle.Exclamation, "ALERTA")
+        'Msgbox("Hola esto es una alerta", MsgboxStyle.Exclamation, "ALERTA")
 
         'Esto genera un cuadro de multiple opción (Cancelar, volver, ignorar)
-        'Msgbox("Hola elija una opción", 'MsgboxStyle.AbortRetryIgnore, "MULTIPLE OPCIÓN")
+        'Msgbox("Hola elija una opción", MsgboxStyle.AbortRetryIgnore, "MULTIPLE OPCIÓN")
 
         'Esto genera un primer plano
-        'Msgbox("Hola esto es un primer plano", 'MsgboxStyle.'MsgboxSetForeground, "PRIMER PLANO")
+        'Msgbox("Hola esto es un primer plano", MsgboxStyle.'MsgboxSetForeground, "PRIMER PLANO")
 
         'Esto genera un cuadro de información
-        'Msgbox("Hola esto es un cuadro de información", 'MsgboxStyle.Information, "INFORMACIÓN")
+        'Msgbox("Hola esto es un cuadro de información", MsgboxStyle.Information, "INFORMACIÓN")
 
         'Esto genera un cuadro de ayuda
-        'Msgbox("Hola esto es un cuadro de ayuda", 'MsgboxStyle.'MsgboxHelp, "AYUDA")
+        'Msgbox("Hola esto es un cuadro de ayuda", MsgboxStyle.'MsgboxHelp, "AYUDA")
 
         'Esto es un cuadro de confirmación
-        'Msgbox("Hola esto es un cuadro de confirmación", 'MsgboxStyle.YesNo, "CONFIRMACIÓN")
+        'Msgbox("Hola esto es un cuadro de confirmación", MsgboxStyle.YesNo, "CONFIRMACIÓN")
 
         '
         'Msgbox("Hol", 'MsgboxStyle.ApplicationModal, "")
@@ -89,14 +89,103 @@
         'MsgBox(operacion.ToString)
 
         'Cambiando valores de formulario por código
+
+
+        'OPERADORES COMPARATIVOS
+        Dim comparo As Boolean
+
+        'Tenemos mayor que >, menor que <,
+        'mayor igual >=, menor igual <=, diferente <>
+        comparo = "hola" = "hola"
+        comparo = (2 + 3) < (2 + 4)
+        comparo = 2 = 4
+
+        MsgBox(comparo)
+
     End Sub
 
-    Private Sub nombre_TextChanged(sender As Object, e As EventArgs) Handles nombre.TextChanged
+    Private Sub nombre_TextChanged(sender As Object, e As EventArgs)
 
         'Modificando formulario por medio de código
         nombre.Text = "Nuevo texto"
         nombre.TextAlign = HorizontalAlignment.Right
         'Haciendo que el cuadro no sea modificable
         nombre.ReadOnly = True
+    End Sub
+
+    Private Sub boton_Click(sender As Object, e As EventArgs)
+
+        'Programando eventos del botón
+        MsgBox("La información ha sido guardada", MsgBoxStyle.Information, "Botón de guardar")
+
+    End Sub
+
+    Private Sub sumar_Click(sender As Object, e As EventArgs)
+
+        'El evento de este botón gestionara la suma entre dos números ingresados por el usuario
+        Dim num1, num2, suma As Integer
+
+        'Asignamos el valor digitado en los cuadros de texto a las variables
+        num1 = numero1.Text
+        num2 = numero2.Text
+
+        suma = num1 + num2
+
+        MsgBox(suma)
+
+    End Sub
+
+    Private Sub cierro_Click(sender As Object, e As EventArgs) Handles cierro.Click
+        'Este comando permitira cerrar la ventana al ser oprimido
+        Me.Close()
+    End Sub
+
+    Private Sub igual_Click(sender As Object, e As EventArgs) Handles igual.Click
+
+        Dim igual As Boolean
+
+        igual = numero1.Text = numero2.Text
+        MsgBox(igual)
+
+    End Sub
+
+    Private Sub diferente_Click(sender As Object, e As EventArgs) Handles diferente.Click
+
+        Dim diferent As Boolean
+
+        diferent = numero1.Text <> numero2.Text
+        MsgBox(diferent)
+    End Sub
+
+    Private Sub mayorque_Click(sender As Object, e As EventArgs) Handles mayorque.Click
+
+        Dim mayor As Boolean
+        'El numero 1 es mayor que el 2
+        mayor = numero1.Text > numero2.Text
+        MsgBox(mayor)
+    End Sub
+
+    Private Sub menorque_Click(sender As Object, e As EventArgs) Handles menorque.Click
+
+        Dim menor As Boolean
+
+        menor = numero1.Text < numero2.Text
+        MsgBox(menor)
+    End Sub
+
+    Private Sub mayorigual_Click(sender As Object, e As EventArgs) Handles mayorigual.Click
+
+        Dim mayori As Boolean
+
+        mayori = numero1.Text >= numero2.Text
+        MsgBox(mayori)
+    End Sub
+
+    Private Sub menorigual_Click(sender As Object, e As EventArgs) Handles menorigual.Click
+
+        Dim menori As Boolean
+
+        menori = numero1.Text <= numero2.Text
+        MsgBox(menori)
     End Sub
 End Class
