26fce751b2d70ce0824cb75c15dd5bb6a9fe00ed
Jhonattan Florez <Jdflorez038@misena.edu.co>
2023-03-21 22:07:39 +0100

    Programando eventos de un menú

--- aprendo2/Form1.Designer.vb
+++ aprendo2/Form1.Designer.vb
@@ -23,15 +23,54 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AccederToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FromulariosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Formulario2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DimeHolaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CerrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AccederToolStripMenuItem, Me.CerrarToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Size = New System.Drawing.Size(506, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AccederToolStripMenuItem
+        '
+        Me.AccederToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FromulariosToolStripMenuItem, Me.DimeHolaToolStripMenuItem})
+        Me.AccederToolStripMenuItem.Name = "AccederToolStripMenuItem"
+        Me.AccederToolStripMenuItem.Size = New System.Drawing.Size(62, 20)
+        Me.AccederToolStripMenuItem.Text = "Acceder"
+        '
+        'FromulariosToolStripMenuItem
+        '
+        Me.FromulariosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Formulario2ToolStripMenuItem})
+        Me.FromulariosToolStripMenuItem.Name = "FromulariosToolStripMenuItem"
+        Me.FromulariosToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FromulariosToolStripMenuItem.Text = "Fromularios"
+        '
+        'Formulario2ToolStripMenuItem
+        '
+        Me.Formulario2ToolStripMenuItem.Name = "Formulario2ToolStripMenuItem"
+        Me.Formulario2ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Formulario2ToolStripMenuItem.Text = "Formulario2"
+        '
+        'DimeHolaToolStripMenuItem
+        '
+        Me.DimeHolaToolStripMenuItem.Name = "DimeHolaToolStripMenuItem"
+        Me.DimeHolaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.DimeHolaToolStripMenuItem.Text = "Dime hola"
+        '
+        'CerrarToolStripMenuItem
+        '
+        Me.CerrarToolStripMenuItem.Name = "CerrarToolStripMenuItem"
+        Me.CerrarToolStripMenuItem.Size = New System.Drawing.Size(51, 20)
+        Me.CerrarToolStripMenuItem.Text = "Cerrar"
         '
         'Form1
         '
@@ -43,10 +82,17 @@ Partial Class Form1
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AccederToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FromulariosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Formulario2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DimeHolaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CerrarToolStripMenuItem As ToolStripMenuItem
 End Class
--- aprendo2/Form1.vb
+++ aprendo2/Form1.vb
@@ -1,5 +1,26 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
+    End Sub
+
+    Private Sub Formulario2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Formulario2ToolStripMenuItem.Click
+
+        'Forma de enlazar un segundo formulario en le menu
+        Form2.ShowDialog()
+
+    End Sub
+
+    Private Sub DimeHolaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DimeHolaToolStripMenuItem.Click
+
+        'Eventos del menuStrip
+        MsgBox("Hola individuo")
+    End Sub
+
+    Private Sub CerrarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CerrarToolStripMenuItem.Click
+
+        'Terminar y cerrar el formulario
+        Me.Close()
+
     End Sub
 End Class
